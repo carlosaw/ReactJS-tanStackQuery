f5b1a2756ea6e4723365f5cb0afc55e741a267e6
carlosaw <carlosfreevg@gmail.com>
2023-10-13 21:27:25 -0400

    Organization

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,20 @@
 "use client";
 
-import { Post } from "@/types/Post";
-import { useQuery } from "@tanstack/react-query";
-import axios from "axios";
+import { usePosts } from "@/utils/queries";
 
 const Page = () => {
   
-  const query = useQuery({
-    queryKey: ['posts'],
-    queryFn: async (): Promise<Post[]> => {
-      const result = await axios.get('https://jsonplaceholder.typicode.com/posts');
-      return result.data;
-    }
-  });
-
+  const posts = usePosts() ;
 
   return (   
     <div className="container mx-auto">
       <h1 className="text-white text-3xl">Opa, tudo bem?</h1>
 
-      {query.isLoading && "Carregando..."}
+      {posts.isLoading && "Carregando..."}
 
-      {query.data &&
+      {posts.data &&
         <ul>
-          {query.data.map(item => (
+          {posts.data.map(item => (
             <li key={item.id}>{item.title}</li>
           ))}
         </ul>
---
+++ src/utils/queries.ts
@@ -0,0 +1,12 @@
+import { useQuery } from "@tanstack/react-query";
+import { getPost, getPosts } from "./api";
+
+export const usePosts = () => {
+   const query = useQuery({ queryKey: ['posts'], queryFn: getPosts });
+   return query;
+}
+
+export const usePost = (id: number) => useQuery({
+  queryKey: ['posts', id],
+  queryFn: () =>  getPost(id)
+});
